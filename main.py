399b0ef37bcef3ab91c9aba1669b56429bcebd26
Odion Obanor <odion900@gmail.com>
2026-09-10 01:36:17 +0100

    fix: oauth2 tokenUrl

--- main.py
+++ main.py
@@ -46,7 +46,8 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkeychangeit")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 # 1 day
-oauth2 = OAuth2PasswordBearer(tokenUrl="token")
+# oauth2 = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
 pwd = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 scheduler = BackgroundScheduler()
 
